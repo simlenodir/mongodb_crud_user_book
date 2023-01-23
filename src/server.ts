6274bd5ebef6dbe0e15ea76c5370851a426d95bf
simlenodir <nodirsmailov0@gmail.com>
2023-01-23 15:06:31 +0300

    application started

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Ok");
+import express, { Application } from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 7777
+const app: Application = express()
+
+app.listen(PORT, ():void => {
+    console.log(`Aplication is started ${PORT}`);
+    
+})
